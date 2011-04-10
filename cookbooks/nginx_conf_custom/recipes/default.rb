5bfc4f79403f80478d5ef4caf01e4f647f154873
Hiro Asari <asari.ruby@gmail.com>
2011-04-10 12:08:21 -0400

    app_name should live inside node[:applications].

--- cookbooks/nginx_conf_custom/recipes/default.rb
+++ cookbooks/nginx_conf_custom/recipes/default.rb
@@ -1,15 +1,18 @@
 # apply nginx customizations if appropriate
 
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
-  remote_file "/data/nginx/servers/#{app_name}/custom.locations.conf" do
-    # do not replace existing customizations
-    not_if "grep -v 'Custom locations' custom.locations.conf", :cwd => "/data/nginx/servers/#{app_name}"
-    mode "0644"
-    source "custom.locations.conf"
-  end
-  
-  execute 'reload nginx configurations' do
-    command '/etc/init.d/nginx reload'
-    user 'root'
+  node[:applications].each do |app_name, data|
+    # presently, we apply the same customizations for all applications on this server
+    remote_file "/data/nginx/servers/#{app_name}/custom.locations.conf" do
+      # do not replace existing customizations
+      not_if "grep -v 'Custom locations' /data/nginx/servers/#{app_name}/custom.locations.conf"
+      mode "0644"
+      source "custom.locations.conf"
+    end
+
+    execute 'reload nginx configurations' do
+      command '/etc/init.d/nginx reload'
+      user 'root'
+    end
   end
 end

0c3c363f4c22d52c05e511be39085074d3a7b601
Hiro Asari <asari.ruby@gmail.com>
2012-02-13 11:31:33 -0500

    use remote_file to overwrite the files. also, fix the source directory

--- cookbooks/nginx-custom/recipes/default.rb
+++ cookbooks/nginx-custom/recipes/default.rb
@@ -1,15 +1,13 @@
 ## note that the file paths are hardcoded.
 if %w(solo app_master app).include? node[:instance_role]
-  file "/data/nginx/servers/todo/custom.conf" do
-    action :create_if_missing
+  remote_file "/data/nginx/servers/todo/custom.conf" do
     owner node.engineyard.ssh_username
     group node.engineyard.ssh_username
     mode 0644
     source "custom.conf"
   end
   
-  file "/data/nginx/servers/todo/custom.ssl.conf" do
-    action :create_if_missing
+  remote_file "/data/nginx/servers/todo/custom.ssl.conf" do
     owner node.engineyard.ssh_username
     group node.engineyard.ssh_username
     mode 0644

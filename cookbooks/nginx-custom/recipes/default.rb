4d755531b1ffc5cc69d4d1d5dc84acf1ebc2ef98
Hiro Asari <asari.ruby@gmail.com>
2012-02-13 10:32:25 -0500

    hard code the nginx conf file paths

--- cookbooks/nginx-custom/recipes/default.rb
+++ cookbooks/nginx-custom/recipes/default.rb
@@ -1,5 +1,6 @@
+## note that the file paths are hardcoded.
 if %w(solo app_master app).include? node[:instance_role]
-  file "/data/nginx/servers/#{app_name}/custom.conf" do
+  file "/data/nginx/servers/todo/custom.conf" do
     action :create_if_missing
     owner node.engineyard.ssh_username
     group node.engineyard.ssh_username
@@ -7,7 +8,7 @@ if %w(solo app_master app).include? node[:instance_role]
     source "custom.conf"
   end
   
-  file "/data/nginx/servers/#{app_name}/custom.ssl.conf" do
+  file "/data/nginx/servers/todo/custom.ssl.conf" do
     action :create_if_missing
     owner node.engineyard.ssh_username
     group node.engineyard.ssh_username

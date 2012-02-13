8d13a942fb34724b6b01b66beedf989874968c0c
Hiro Asari <asari.ruby@gmail.com>
2012-02-13 10:20:16 -0500

    execute this recipe only on app servers

--- cookbooks/nginx-custom/recipes/default.rb
+++ cookbooks/nginx-custom/recipes/default.rb
@@ -1,15 +1,17 @@
-file "/data/nginx/servers/#{app}/custom.conf" do
-  action :create_if_missing
-  owner node.engineyard.ssh_username
-  group node.engineyard.ssh_username
-  mode 0644
-  source "custom.conf"
-end
-
-file "/data/nginx/servers/#{app}/custom.ssl.conf" do
-  action :create_if_missing
-  owner node.engineyard.ssh_username
-  group node.engineyard.ssh_username
-  mode 0644
-  source "custom.ssl.conf"
+if %w(solo app_master app).include? node[:instance_role]
+  file "/data/nginx/servers/#{app_name}/custom.conf" do
+    action :create_if_missing
+    owner node.engineyard.ssh_username
+    group node.engineyard.ssh_username
+    mode 0644
+    source "custom.conf"
+  end
+  
+  file "/data/nginx/servers/#{app_name}/custom.ssl.conf" do
+    action :create_if_missing
+    owner node.engineyard.ssh_username
+    group node.engineyard.ssh_username
+    mode 0644
+    source "custom.ssl.conf"
+  end
 end

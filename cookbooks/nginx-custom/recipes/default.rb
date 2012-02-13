44e29c34b1e0fcc6ad85da63c4ab14bbb09672d8
Hiro Asari <asari.ruby@gmail.com>
2012-02-13 10:07:25 -0500

    set up custom.ssl.conf as well

--- cookbooks/nginx-custom/recipes/default.rb
+++ cookbooks/nginx-custom/recipes/default.rb
@@ -1,6 +1,15 @@
-  file "/data/nginx/servers/#{app}/custom.conf" do
-    action :create_if_missing
-    owner node.engineyard.ssh_username
-    group node.engineyard.ssh_username
-    mode 0644
-  end
+file "/data/nginx/servers/#{app}/custom.conf" do
+  action :create_if_missing
+  owner node.engineyard.ssh_username
+  group node.engineyard.ssh_username
+  mode 0644
+  source "custom.conf"
+end
+
+file "/data/nginx/servers/#{app}/custom.ssl.conf" do
+  action :create_if_missing
+  owner node.engineyard.ssh_username
+  group node.engineyard.ssh_username
+  mode 0644
+  source "custom.ssl.conf"
+end

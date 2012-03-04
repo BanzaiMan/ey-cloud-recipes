426fb0667f6dde7e819be5e42b98f8bdde80fc37
Hiro Asari <asari.ruby@gmail.com>
2012-03-04 17:44:00 -0500

    add custom_database_yml recipe

---
+++ cookbooks/custom_database_yml/recipes/default.rb
@@ -0,0 +1,19 @@
+environment = node.engineyard.environment
+
+db_master = node[:instances].select {|i| i["role"] == "db_master"}.first
+user = node[:users].first
+
+node.engineyard.apps.each do |app|
+	template "/data/#{app.name}/current/config/database.yml" do
+		source "database.yml.erb"
+		owner environment.ssh_username
+		group environment.ssh_username
+		mode 0744
+		variables({
+			:username => user[:username],
+			:password => user[:password],
+			:dbname => app.database_name,
+			:host => environment.db_host
+			})
+	end
+end
